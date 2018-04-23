4be0703cb629983c297e4c5b219d3694c901eec8
杨操 <yangcao@boco.com.cn>
2018-04-23 11:09:41 +0800

    i24x

---
+++ eureka-provider1/src/main/java/com/github/caoyeung/controller/EmailController.java
@@ -0,0 +1,99 @@
+package com.github.caoyeung.controller;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.github.caoyeung.base.BaseController;
+import com.github.caoyeung.controller.fegin.UserFeginClient;
+import com.github.caoyeung.model.Email;
+import com.github.caoyeung.util.HttpUtil;
+import com.github.caoyeung.util.Mail_163Util;
+
+@Controller
+public class EmailController extends BaseController {
+	private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
+	@Autowired
+	UserFeginClient userFeginClient;
+	public static final String API_PREFIX = "/EmailController";
+
+	// @TxTransaction(isStart=true)
+	@RequestMapping(value = API_PREFIX + "/doEmail", method = RequestMethod.GET)
+	@ResponseBody
+	public String sendSystemEmail() {
+//		HttpUtil.newRunTimeExceptionOrSleep(false,0);
+		String result = "SUCESS";
+		 result = Mail_163Util.sendEmail("cyang198906@163.com",
+		 new String[]{
+		 // "610039525@qq.com",
+		 "2406352526@qq.com",
+		 // "yangcao@boco.com.cn",
+		 // "1938027689@qq.com"
+		 },
+		 "yc535689",
+		 "【Jmail_163邮箱发送测试】",
+		 "Jmail_163邮箱发送测试......");
+		return result;
+	}
+	
+	
+	
+	@RequestMapping(value = API_PREFIX + "/sendPrizeEmail", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody Email sendPrizeEmail( @RequestBody Email email,HttpServletRequest request,HttpServletResponse response) {
+		email.setContent("尊敬的用户，恭喜你抽中移动9999元感恩回馈大礼包！");
+		email.setTitle("【中奖通知】");
+		Mail_163Util.sendEmail("cyang198906@163.com", new String[] {
+//		         "610039525@qq.com",
+				email.getDestnation(),
+				// "yangcao@boco.com.cn",
+				// "1938027689@qq.com"
+				}, "yc535689", email.getTitle(), email.getContent());
+
+		return email;
+	}
+	
+	//@RequestBody 对GET请求无效，请注意 Content-Type
+	@RequestMapping(value = API_PREFIX + "/sendPrizeEmailMulti", method = {RequestMethod.POST })
+	public @ResponseBody Email sendPrizeEmailMulti(@RequestBody Email email,@RequestParam("phone") String phone,HttpServletRequest request,HttpServletResponse response) {
+		email.setContent("尊敬的用户，恭喜你抽中移动9999元感恩回馈大礼包！");
+		email.setTitle("【中奖通知】");
+		email.setPhone(phone);
+		HttpUtil.newRunTimeExceptionOrSleep(false,false,10);
+		Mail_163Util.sendEmail("cyang198906@163.com", new String[] {
+//		         "610039525@qq.com",
+				email.getDestnation(),
+				// "yangcao@boco.com.cn",
+				 "1938027689@qq.com"
+				}, "yc535689", email.getTitle(), email.getContent());
+
+		return email;
+	}
+	
+	@RequestMapping(value = API_PREFIX + "/sendPrizeEmailStr", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody Email sendPrizeEmailStr(@RequestParam("email") String emailStr,HttpServletRequest request,HttpServletResponse response) {
+		Email email = JSON.parseObject(emailStr, Email.class);
+		email.setContent("尊敬的用户，恭喜你抽中移动9999元感恩回馈大礼包！");
+		email.setTitle("【中奖通知】");
+//		HttpUtil.newRunTimeExceptionOrSleep(false,0);
+		Mail_163Util.sendEmail("cyang198906@163.com", new String[] {
+//		         "610039525@qq.com",
+				email.getDestnation(),
+				// "yangcao@boco.com.cn",
+				// "1938027689@qq.com"
+				}, "yc535689", email.getTitle(), email.getContent());
+
+		return email;
+	}
+}

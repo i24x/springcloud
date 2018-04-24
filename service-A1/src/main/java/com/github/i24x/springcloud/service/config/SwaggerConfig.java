454aacaf3727131bfdfce420859e7bc0eb6cbdcf
杨操 <yangcao@boco.com.cn>
2018-04-24 18:16:56 +0800

    杨操 <yangcao@boco.com.cn>

--- service-A1/src/main/java/com/github/i24x/springcloud/service/config/SwaggerConfig.java
+++ service-A1/src/main/java/com/github/i24x/springcloud/service/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package com.github.i24x.springcloud.service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.async.DeferredResult;
-
-import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,55 +12,22 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Swagger2配置类
- */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	/**
-	 * 可以定义多个组，比如本类中定义把test和demo区分开了 （访问页面就可以看到效果了）
-	 */
+	private static final String BASEPACKAGE = "com.github.i24x";
 	@Bean
 	public Docket createInstanceDocket() {
-		Docket docket = new Docket(DocumentationType.SWAGGER_2)
-				.select().apis(RequestHandlerSelectors.basePackage("com.github.i24x.springcloud"))
-//		        .apis(RequestHandlerSelectors.basePackage("com.github.caoyeung"))
-		        .paths(PathSelectors.any())
-				.build()
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage(BASEPACKAGE)).paths(PathSelectors.any()).build()
 				.apiInfo(createInstanceApiInfo());
 		return docket;
 	}
+
 	private ApiInfo createInstanceApiInfo() {
-		return new ApiInfoBuilder().title("XX项目测试")
-				.description("用于XXX模块1")
-				.version("1.0")
-				.termsOfServiceUrl("http://www.boco.com.cn/")
-				.contact(new Contact("XXX小组", "http://www.boco.com.cn/", "cyang198906@163.com"))
-				.license("XX许可")
-				.licenseUrl("http://www.boco.com.cn/")
-				.build();
+		return new ApiInfoBuilder().title("SPRING CLOUD 环境搭建").description("用于service-A1模块测试").version("1.0")
+				.termsOfServiceUrl("https://github.com/i24x")
+				.contact(new Contact("i24x", "https://github.com/i24x", "i24x610039525@163.com")).license("转载请标明出处")
+				.licenseUrl("https://github.com/i24x").build();
 	}
-	
-
-	/*@Bean
-	public Docket demoApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("server2").genericModelSubstitutes(DeferredResult.class)
-				// .genericModelSubstitutes(ResponseEntity.class)
-				.useDefaultResponseMessages(false).forCodeGeneration(false).pathMapping("/").select()
-				.paths(Predicates.or(PathSelectors.regex("/demo/.*")))// 过滤的接口
-				.build().apiInfo(demoApiInfo());
-	}
-
-	private ApiInfo demoApiInfo() {
-		ApiInfo apiInfo = new ApiInfo("大标题",// 大标题
-				"小标题",// 小标题
-				"版本",// 版本
-				"作者", "CaoYeung",// 作者
-				"链接显示文字",// 链接显示文字
-				"网站链接"// 网站链接
-		);
-
-		return apiInfo;
-	}*/
 }
--- springcloud-ribbon/src/main/java/com/github/i24x/service/ribbon/config/SwaggerConfig.java
+++ springcloud-ribbon/src/main/java/com/github/i24x/service/ribbon/config/SwaggerConfig.java
@@ -16,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	private static final String BASEPACKAGE = "com.github.i24x";
-
 	@Bean
 	public Docket createInstanceDocket() {
 		Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
@@ -26,9 +25,9 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo createInstanceApiInfo() {
-		return new ApiInfoBuilder().title("XX项目测试").description("用于Ribbon模块1").version("1.0")
-				.termsOfServiceUrl("http://www.boco.com.cn/")
-				.contact(new Contact("XXX小组", "http://www.boco.com.cn/", "cyang198906@163.com")).license("XX许可")
-				.licenseUrl("http://www.boco.com.cn/").build();
+		return new ApiInfoBuilder().title("SPRING CLOUD 环境搭建").description("用于Ribbon模块测试").version("1.0")
+				.termsOfServiceUrl("https://github.com/i24x")
+				.contact(new Contact("i24x", "https://github.com/i24x", "i24x610039525@163.com")).license("转载请标明出处")
+				.licenseUrl("https://github.com/i24x").build();
 	}
 }

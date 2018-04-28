c0cffe3ebb8c2d68490f46862f3a17fa199c19f9
caoyeung <i24x610039525@163.com>
2018-04-28 18:00:43 +0800

    caoyeung <i24x610039525@163.com>

--- service-A1/src/main/java/com/github/i24x/springcloud/service/config/SwaggerConfig.java
+++ service-A1/src/main/java/com/github/i24x/springcloud/service/config/SwaggerConfig.java
@@ -18,16 +18,24 @@ public class SwaggerConfig {
 	private static final String BASEPACKAGE = "com.github.i24x";
 	@Bean
 	public Docket createInstanceDocket() {
-		Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage(BASEPACKAGE)).paths(PathSelectors.any()).build()
-				.apiInfo(createInstanceApiInfo());
+		Docket docket = new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.basePackage(BASEPACKAGE))
+			.paths(PathSelectors.any())
+			.build()
+			.apiInfo(createInstanceApiInfo());
 		return docket;
 	}
 
 	private ApiInfo createInstanceApiInfo() {
-		return new ApiInfoBuilder().title("SPRING CLOUD 环境搭建").description("用于service-A1模块测试").version("1.0")
-				.termsOfServiceUrl("https://github.com/i24x")
-				.contact(new Contact("i24x", "https://github.com/i24x", "i24x610039525@163.com")).license("转载请标明出处")
-				.licenseUrl("https://github.com/i24x").build();
+		return new ApiInfoBuilder()
+			.title("解析 SPRING CLOUD")
+			.description("service-A1模块")
+			.version("1.0")
+			.termsOfServiceUrl("https://github.com/i24x")
+			.contact(new Contact("i24x", "https://github.com/i24x", "i24x610039525@163.com"))
+			.license("转载请标明出处")
+			.licenseUrl("https://github.com/i24x")
+			.build();
 	}
 }
